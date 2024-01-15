721fc99f17852501535e91e7fa4d8465432d38a4
GiaTrong <hoanggiatrang01@gmail.com>
2024-01-15 14:23:42 +0700

    trang admin tổng quan

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 // routes
 const routeClient = require("./routes/client/index");
+const roteAdmin = require('./routes/admin/index')
 // pug
 const pug = require("pug");
 // database
 const database = require("./config/database");
+// systemConfig
+const systemConfig = require('./config/system')
 // DOTENV
 require("dotenv").config();
 
@@ -24,6 +27,11 @@ app.use(express.static("public"));
 
 // routes
 routeClient(app);
+roteAdmin(app);
+
+//  APP VARIABLES LOCALS => thiết lập biến toàn cục cho TẤT CẢ FILE BUG của bạn
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,3 +1,13 @@
-module.exports = () => {
-    
+const systemConfig = require('../../config/system')
+
+const dashboardRouter = require('./dashboard.router')
+const productsRouter = require('./products.router')
+
+module.exports = (app) => {
+    const ADMIN_PATH = systemConfig.prefixAdmin
+
+    // use router
+    app.use(ADMIN_PATH + "/dashboard", dashboardRouter);
+
+    app.use(ADMIN_PATH + "/products", productsRouter);
 }

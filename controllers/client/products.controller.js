adac78aa18cfbb5f16f06229e785f6e6f64e458a
GiaTrong <hoanggiatrang01@gmail.com>
2024-01-15 13:13:48 +0700

    tính năng sản phẩm bên client

--- controllers/client/products.controller.js
+++ controllers/client/products.controller.js
@@ -1,6 +1,11 @@
+const Product = require("../../model/product.model")
 // [GET]
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const product = await Product.find({
+        deleted: false,
+    })
+    
     res.render("client/pages/products/index.pug", {
-        products: {}
+        products: product
     });
 }

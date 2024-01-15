0e902b019a0557fa79436e520df91c39018569c7
GiaTrong <hoanggiatrang01@gmail.com>
2024-01-15 14:30:18 +0700

    lấy data in ra sản phẩm

--- controllers/admin/products.controller.js
+++ controllers/admin/products.controller.js
@@ -1,6 +1,15 @@
+const Prdoduct = require('../../model/product.model')
 // [GET] /admin/dashboard
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    // give a data
+    const products = await Prdoduct.find({
+        deleted: false,
+    })
+
+    console.log(products)
+
   res.render("admin/pages/product/index", {
     pageTitle: "Trang san pham",
+    products: products,
   });
 };
